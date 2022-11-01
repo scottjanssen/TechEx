40328df1c1a99e29ca1d58dd0ba1c9a1e1c913ef
Balaram Das Behera <balaramdb@gmail.com>
2022-11-01 19:33:00 -0400

    added logo icon to main page

--- react/src/Header.js
+++ react/src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import logo from './media/icon-2.png'
 
 const Header = () => {
   return (
     <div className='container'>
-      <div className='header'>
-        <h1 className='title'>TECHEX</h1>
+      <div className='row header'>
+        <div className='row'>
+          <img src={ logo } alt='Up-down arrow icon' width='85px' />
+          <h1 className='title'>TECHEX</h1>
+        </div>
       </div>
     </div>
   )
