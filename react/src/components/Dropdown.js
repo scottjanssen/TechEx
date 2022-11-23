1fc353d7738edfc2d7b4dad9aee42095cf2342f7
Scott Janssen <scottjanssen@Scotts-MacBook-Air.local>
2022-11-23 11:55:35 -0500

    Added consistent spacing to text boxes

--- react/src/components/Dropdown.js
+++ react/src/components/Dropdown.js
@@ -70,114 +70,120 @@ export default function Dropdown(props) {
 
   return (
     <div>
-    <center><Stack direction="row" spacing={3}>
-      <div className='base'>
-        <Box sx={{ minWidth: 120 }}>
-          <TextField
-            SelectProps={{style: {color: 'white'}, }}
-            value={base}
-            onChange={handleChange}
-            variant="outlined"
-            label="Base"
-            name={'base'}
-            InputLabelProps={{style: { color: '#f5756b' },}}
-            sx={{"& .MuiOutlinedInput-root": {"& > fieldset": { borderColor: "#f5756b" },},}}
-            select
-            required
-            helperText="Please choose a currency"
-            FormHelperTextProps={{style: { color: 'white' },}}
-          >
-            {currenciesArray.map(x => <MenuItem key={`${x}-base`} value={x} data-cy={`select-option-${x.ISO}`}>{x.symbol+ " " + x.ISO}</MenuItem>)}
-          </TextField>
-        </Box>
-      </div>
+        <Stack direction={"column"} spacing={3}>
+        <center>
+        <Stack direction="row" spacing={3}>
+            <div className='base'>
+                <Box sx={{ minWidth: 120 }}>
+                    <TextField
+                        SelectProps={{style: {color: 'white'}, }}
+                        value={base}
+                        onChange={handleChange}
+                        variant="outlined"
+                        label="Base"
+                        name={'base'}
+                        InputLabelProps={{style: { color: '#f5756b' },}}
+                        sx={{"& .MuiOutlinedInput-root": {"& > fieldset": { borderColor: "#f5756b" },},}}
+                        select
+                        required
+                        helperText="Please choose a currency"
+                        FormHelperTextProps={{style: { color: 'white' },}}
+                      >
+                        {currenciesArray.map(x => <MenuItem key={`${x}-base`} value={x} data-cy={`select-option-${x.ISO}`}>{x.symbol+ " " + x.ISO}</MenuItem>)}
+                    </TextField>
+                </Box>
+            </div>
 
-      <div className='value'>
-        <Box
-        component="form"
-        sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}
-        noValidate
-        autoComplete="off"
-        >
-          <TextField
-            required
-            className={'value-textbox'}
-            label="Value"
-            name={"value"}
-            value={input}
-            onChange= {handleChange3}
-            InputLabelProps={{style: { color: '#f5756b' },}}
-            inputProps={{ style: { color: "white", textAlign:'center'} }}
-            sx={{"& .MuiOutlinedInput-root": {"& > fieldset": { borderColor: "#f5756b" },},}}
-            variant="outlined"
-            helperText="Please input your amount to exchange"
-            FormHelperTextProps={{style: { color: 'white' },}}
-          />
-        </Box>
-      </div>
-      </Stack></center>
+            <div className='value'>
+                <Box
+                    component="form"
+                    sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}
+                    noValidate
+                    autoComplete="off"
+                >
+                  <TextField
+                    required
+                    className={'value-textbox'}
+                    label="Value"
+                    name={"value"}
+                    value={input}
+                    onChange= {handleChange3}
+                    InputLabelProps={{style: { color: '#f5756b' },}}
+                    inputProps={{ style: { color: "white", textAlign:'center'} }}
+                    sx={{"& .MuiOutlinedInput-root": {"& > fieldset": { borderColor: "#f5756b" },},}}
+                    variant="outlined"
+                    helperText="Please input your amount to exchange"
+                    FormHelperTextProps={{style: { color: 'white' },}}
+                  />
+                </Box>
+            </div>
+        </Stack>
+    </center>
 
-      <center><Stack direction="row" spacing={1.5}>
-      <div className='target'>
-        <Box sx={{ minWidth: 120 }}>
-          <TextField
-          SelectProps={{style: {color: 'white'}, }}
-          value={target}
-          onChange={handleChange2}
-          variant="outlined"
-          label="Target"
-          name={'target'}
-          InputLabelProps={{style: { color: '#f5756b' },}}
-          sx={{"& .MuiOutlinedInput-root": {"& > fieldset": { borderColor: "#f5756b" },},}}
-          select
-          required
-          FormHelperTextProps={{style: { color: 'white' },}}
-          helperText="Please choose a currency"
-          >
-            {currenciesArray.map(x => <MenuItem key={`${x}-target`} value={x} data-cy={`select-option-${x.ISO}`}>{x.symbol+ " " + x.ISO}</MenuItem>)}
-          </TextField>
-        </Box>
-      </div>
-    <div className='result'>
-      <Box
-        component="form"
-        sx={{'& .MuiTextField-root': { m: 1, width: '25ch'}, '& .MuiTextField-root.Mui-disabled': {color: 'white'}}}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-            variant="outlined"
-            value={result}
-            label="Result"
-            disabled
-            id={"result-textbox"}
-            name={'result-textbox'}
-            InputLabelProps={{style: { color: '#f5756b' },}}
-            inputProps={{ style: { color: "white", textAlign:'center'} }}
-            sx={{"& .MuiInputBase-root.Mui-disabled": {
-                "& > fieldset": {
-                    borderColor: "#f5756b",
-                }},
-                color: "white"
-            }}
-            helperText="Your exchange result will appear here"
-            FormHelperTextProps={{style: { color: 'white' },}}
-        >{result}</TextField>
-      </Box>
-    </div>
-    </Stack></center>
-    <Stack>
-      <div className='submit'>
-        <Button 
-        variant="outlined"
-        color='warning'
-        onClick={ fetchData }
-        name={'submit-button'}
-        >
-          Submit
-        </Button>
-      </div>
-      </Stack>
+        <center>
+            <Stack direction="row" spacing={3}>
+                <div className='target'>
+                    <Box sx={{ minWidth: 120 }}>
+                      <TextField
+                      SelectProps={{style: {color: 'white'}, }}
+                      value={target}
+                      onChange={handleChange2}
+                      variant="outlined"
+                      label="Target"
+                      name={'target'}
+                      InputLabelProps={{style: { color: '#f5756b' },}}
+                      sx={{"& .MuiOutlinedInput-root": {"& > fieldset": { borderColor: "#f5756b" },},}}
+                      select
+                      required
+                      FormHelperTextProps={{style: { color: 'white' },}}
+                      helperText="Please choose a currency"
+                      >
+                        {currenciesArray.map(x => <MenuItem key={`${x}-target`} value={x} data-cy={`select-option-${x.ISO}`}>{x.symbol+ " " + x.ISO}</MenuItem>)}
+                      </TextField>
+                    </Box>
+              </div>
+              <div className='result'>
+                  <Box
+                    component="form"
+                    sx={{'& .MuiTextField-root': { m: 1, width: '25ch'}, '& .MuiTextField-root.Mui-disabled': {color: 'white'}}}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField
+                        variant="outlined"
+                        value={result}
+                        label="Result"
+                        disabled
+                        id={"result-textbox"}
+                        name={'result-textbox'}
+                        InputLabelProps={{style: { color: '#f5756b' },}}
+                        inputProps={{ style: { color: "white", textAlign:'center'} }}
+                        sx={{"& .MuiInputBase-root.Mui-disabled": {
+                            "& > fieldset": {
+                                borderColor: "#f5756b",
+                            }},
+                            color: "white"
+                        }}
+                        helperText="Your exchange result will appear here"
+                        FormHelperTextProps={{style: { color: 'white' },}}
+                    >{result}</TextField>
+                  </Box>
+              </div>
+        </Stack>
+        </center>
+        <Stack>
+            <div className='submit'>
+                <Button
+                variant="outlined"
+                color='warning'
+                onClick={ fetchData }
+                name={'submit-button'}
+                >
+                  Submit
+                </Button>
+             </div>
+        </Stack>
+        </Stack>
   </div>
   );
 }
